35cde22e4ba298b960673a3dd105192e806abf5c
Khushi <you@example.com>
2025-03-05 18:44:28 +0530

    changed dynamodb tables

--- khushi-api-review/src/main/java/therapyapp/infrastructure/DynamoDbStack.java
+++ khushi-api-review/src/main/java/therapyapp/infrastructure/DynamoDbStack.java
@@ -1,37 +1,192 @@
 package therapyapp.infrastructure;
 
-
-import software.amazon.awscdk.Stack;
-import software.constructs.Construct;
-import software.amazon.awscdk.services.dynamodb.*;
+import software.amazon.awscdk.App;
 import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.GlobalSecondaryIndexProps;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.constructs.Construct;
 
 public class DynamoDbStack extends Stack {
-
-    public final Table userTable;
-    public final Table journalTable;
-    public final Table sessionTable;
-    public final Table messageTable;
-    public final Table appointmentTable;
-
-    public DynamoDbStack(final Construct scope, final String id) {
-        super(scope, id);
-
-        userTable = createTable("UserTable", "userId");
-        journalTable = createTable("JournalTable", "journalId");
-        sessionTable = createTable("SessionTable", "sessionId");
-        messageTable = createTable("MessageTable", "messageId");
-        appointmentTable = createTable("AppointmentTable", "appointmentId");
-    }
-
-    private Table createTable(String tableName, String partitionKeyName) {
-        return Table.Builder.create(this, tableName)
-                .tableName(tableName)
+    
+    public final Table usersTable;
+    public final Table journalsTable;
+    public final Table therapistsTable;
+    public final Table mappingTable;
+    public final Table journalAccessTable;
+    public final Table sessionsTable;
+    public final Table messagesTable;
+    public final Table appointmentsTable;
+    
+    public DynamoDbStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+        
+        
+        usersTable = Table.Builder.create(this, "UsersTable")
+                .tableName("Users")
                 .partitionKey(Attribute.builder()
-                        .name(partitionKeyName)
+                        .name("userId")
                         .type(AttributeType.STRING)
                         .build())
-                .removalPolicy(RemovalPolicy.DESTROY)  // Only for development; use RETAIN for production
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+        
+        usersTable.addGlobalSecondaryIndex(GlobalSecondaryIndexProps.builder()
+                .indexName("email-index")
+                .partitionKey(Attribute.builder()
+                        .name("email")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("userId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .build());
+        
+        
+        journalsTable = Table.Builder.create(this, "JournalsTable")
+                .tableName("Journals")
+                .partitionKey(Attribute.builder()
+                        .name("clientId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("timestamp")
+                        .type(AttributeType.STRING)
+                        .build())
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+        
+    
+        therapistsTable = Table.Builder.create(this, "TherapistsTable")
+                .tableName("Therapists")
+                .partitionKey(Attribute.builder()
+                        .name("therapistId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+        
+        
+        mappingTable = Table.Builder.create(this, "MappingTable")
+                .tableName("TherapistClientMapping")
+                .partitionKey(Attribute.builder()
+                        .name("clientId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("therapistId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    
+        mappingTable.addGlobalSecondaryIndex(GlobalSecondaryIndexProps.builder()
+                .indexName("therapist-index")
+                .partitionKey(Attribute.builder()
+                        .name("therapistId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("clientId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .build());
+        
+        
+        journalAccessTable = Table.Builder.create(this, "JournalAccessTable")
+                .tableName("JournalAccess")
+                .partitionKey(Attribute.builder()
+                        .name("therapistId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("clientId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+        
+        journalAccessTable.addGlobalSecondaryIndex(GlobalSecondaryIndexProps.builder()
+                .indexName("client-index")
+                .partitionKey(Attribute.builder()
+                        .name("clientId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("therapistId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .build());
+        
+        
+        sessionsTable = Table.Builder.create(this, "SessionsTable")
+                .tableName("Sessions")
+                .partitionKey(Attribute.builder()
+                        .name("sessionId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+        
+        sessionsTable.addGlobalSecondaryIndex(GlobalSecondaryIndexProps.builder()
+                .indexName("therapist-session-index")
+                .partitionKey(Attribute.builder()
+                        .name("therapistId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("sessionDate")
+                        .type(AttributeType.STRING)
+                        .build())
+                .build());
+        
+    
+        messagesTable = Table.Builder.create(this, "MessagesTable")
+                .tableName("Messages")
+                .partitionKey(Attribute.builder()
+                        .name("conversationId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("timestamp")
+                        .type(AttributeType.STRING)
+                        .build())
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+        
+        messagesTable.addGlobalSecondaryIndex(GlobalSecondaryIndexProps.builder()
+                .indexName("sender-index")
+                .partitionKey(Attribute.builder()
+                        .name("senderId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("timestamp")
+                        .type(AttributeType.STRING)
+                        .build())
+                .build());
+        
+        
+        appointmentsTable = Table.Builder.create(this, "AppointmentsTable")
+                .tableName("Appointments")
+                .partitionKey(Attribute.builder()
+                        .name("appointmentId")
+                        .type(AttributeType.STRING)
+                        .build())
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
     }
 }
